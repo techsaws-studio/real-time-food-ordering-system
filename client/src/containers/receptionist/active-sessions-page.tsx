c1560da14bbc4aaf2b2f569a296980004e998730
syeddhassannaali <syeddhassannaali@gmail.com>
2025-11-25 00:52:21 +0500

    made changes

--- client/src/containers/receptionist/active-sessions-page.tsx
+++ client/src/containers/receptionist/active-sessions-page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import DashboardPageHeader from "@/components/partials/dashboard-page-header";
+
 function ActiveSessionsPage() {
-  return <main className="bg-white w-full h-full">ActiveSessionsPage</main>;
+  return (
+    <main className="bg-white w-full h-full">
+      <section className="dashboard-layout-standard section-padding-standard flex flex-col gap-8">
+        <DashboardPageHeader title="Active Sessions" />
+      </section>
+    </main>
+  );
 }
 
 export default ActiveSessionsPage;
--- client/src/containers/receptionist/sessions-history-page.tsx
+++ client/src/containers/receptionist/sessions-history-page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import DashboardPageHeader from "@/components/partials/dashboard-page-header";
+
 function SessionsHistoryPage() {
-  return <main className="bg-white w-full h-full">SessionsHistoryPage</main>;
+  return (
+    <main className="bg-white w-full h-full">
+      <section className="dashboard-layout-standard section-padding-standard flex flex-col gap-8">
+        <DashboardPageHeader title="Sessions History" />
+      </section>
+    </main>
+  );
 }
 
 export default SessionsHistoryPage;
